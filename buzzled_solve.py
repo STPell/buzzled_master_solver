dcc077f13ee08453ebcbd3553442b2070b52e392
Samuel Pell <pellsamuel@gmail.com>
2025-06-13 10:36:03 +1200

    Add solver for mini and classic versions using the Z3 solver

---
+++ buzzled_solve.py
@@ -0,0 +1,129 @@
+import z3
+import plot_solutions as plotter
+
+DEPTH_2_TILE_CNT = 19
+DEPTH_2_FILTERS = [
+    [0,4,9,14,18],
+    [2,7,12,17],
+    [5,10,15],
+    [0,2,5],
+    [1,4,7,10],
+    [3,6,9,12,15],
+    [8,11,14,17],
+    [13,16,18],
+    [15,17,18],
+    [10,12,14,16],
+    [5,7,9,11,13],
+    [2,4,6,8],
+    [0,1,3],
+    [3,8,13],
+    [1,6,11,16],
+]
+
+
+DEPTH_3_TILE_CNT = 37
+DEPTH_3_FILTERS = [
+    [0, 4, 11, 18, 25, 32, 36],
+    [2, 8, 15, 22, 29, 35],
+    [5, 12, 19, 26, 33],
+    [9, 16, 23, 30],
+    [0, 2, 5, 9],
+    [1, 4, 8, 12, 16],
+    [3, 7, 11, 15, 19, 23],
+    [6, 10, 14, 18, 22, 26, 30],
+    [13, 17, 21, 25, 29, 33],
+    [20, 24, 28, 32, 35],
+    [27, 31, 34, 36],
+    [30, 33, 35, 36],
+    [23, 26, 29, 32, 34],
+    [16, 19, 22, 25, 28, 31],
+    [9, 12, 15, 18, 21, 24, 27],
+    [5, 8, 11, 14, 17, 20],
+    [2, 4, 7, 10, 13],
+    [0, 1, 3, 6],
+    [6, 13, 20, 27],
+    [3, 10, 17, 24, 31],
+    [1, 7, 14, 21, 28, 34]
+]
+
+def solve_generic(weights, sums, rows, n_tiles):
+    if len(weights) != n_tiles:
+        return None
+
+    if len(sums) != len(rows):
+        return None
+
+    solver = z3.Solver()
+
+    var = [z3.Int(f"tile_{i}") for i in range(n_tiles)]
+    tiles_are_0_or_value = [z3.Or(var[i] == 0, var[i] == weights[i]) for i in range(n_tiles)]
+
+    solver.add(tiles_are_0_or_value)
+
+    for i in range(len(rows)):
+        row = rows[i]
+        s = var[row[0]]
+        for j in range(len(row[1:])):
+            s += var[row[j + 1]]
+
+        #print(s)
+        solver.add(s == sums[i])
+
+    #print(solver)
+    if solver.check() == z3.sat:
+        m = solver.model()
+
+        solution = ""
+        for v in var[::-1]:
+            if m.eval(v).as_long() != 0:
+                solution += "1"
+            else:
+                solution += "0"
+
+        return solution
+    else:
+        return None
+
+
+def solve_classic(w, s):
+    return solve_generic(w, s, DEPTH_3_FILTERS, DEPTH_3_TILE_CNT)
+
+
+def solve_expert(w, s):
+    return solve_generic(w, s, DEPTH_4_FILTERS, DEPTH_4_TILE_CNT)
+
+
+def solve_mini(w, s):
+    return solve_generic(w, s, DEPTH_2_FILTERS, DEPTH_2_TILE_CNT)
+
+
+def test_classic():
+    weights_classic_59 = [2,5,1,3,1,2,3,4,1,2,4,4,4,5,5,5,4,1,3,3,5,5,5,5,1,2,3,3,3,2,2,2,5,4,1,5,5]
+    sums_classic_59 = [5,11,10,11,2,10,21,14,14,9,6,11,6,9,18,12,14,6,11,10,18]
+    solution_classic_59 = "0111011011001101000011111110111111000"
+
+    s = solve_classic(weights_classic_59, sums_classic_59)
+    print(s)
+    print(solution_classic_59)
+    print(s == solution_classic_59)
+
+    plotter.plot_solutions([s], sums=sums_classic_59, weights=weights_classic_59)
+
+
+def test_mini():
+    weights_mini_65 = [1,4,5,3,5,4,1,3,4,1,3,5,2,1,1,3,4,1,3]
+    sums_mini_65 = [3,10,10,10,10,7,6,1,3,6,14,6,5,1,10]
+    solution_mini_65 = "0001111111011100111"
+
+    s = solve_mini(weights_mini_65, sums_mini_65)
+    print(s)
+    print(solution_mini_65)
+    print(s == solution_mini_65)
+
+    #plotter.plot_solutions([s], sums=sums_classic_59, weights=weights_classic_59)
+
+
+
+if __name__ == "__main__":
+    test_classic()
+    test_mini()
